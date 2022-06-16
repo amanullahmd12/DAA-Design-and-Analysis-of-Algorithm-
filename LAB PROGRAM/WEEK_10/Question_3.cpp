090ee92439743785090b55bf58744ac87d95797e
MD AMANULLAH <92320933+amanullahmd12@users.noreply.github.com>
2022-06-16 10:54:16 +0530

    Update and rename Question3.cpp to Question_3.cpp

---
+++ LAB PROGRAM/WEEK_10/Question_3.cpp
@@ -0,0 +1,43 @@
+
+// Given an unsorted array of elements, design an algorithm and implement it using a program to
+// find whether majority element exists or not. Also find median of the array. A majority element is
+// an element that appears more than n/2 times, where n is the size of array.
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+bool majority(vector<int> arr){
+    map<int,int> m;
+    for(int i=0;i<arr.size();i++)
+    m[arr[i]]++;
+    for( auto& i:m){
+        if(i.second>=(arr.size()/2))
+           return true;
+    }
+    return false;  
+}
+int median(vector<int> arr){
+    if(arr.size()%2==0)
+       return (arr[(arr.size()/2)]+arr[(arr.size()/2)-1])/2;
+    else
+       return arr[(arr.size()/2)];
+    
+
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    if(majority(arr))
+        cout<<"\nYES\n";
+    else
+        cout<<"\nNO\n";
+    cout<<median(arr);
+}
